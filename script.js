1a46f09ece5e32d2a50843346a8d76e37a307910
Puffer-dev56 <Pongsapat4848@gmail.com>
2026-06-09 17:51:50 +0700

    Make a Search function and task report

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const search = document.querySelector("#search");
 const addForm = document.querySelector("#addForm");
 const taskFrame = document.querySelector("#taskFrame");
+const labActive = document.querySelector("#labActive");
+const labComplete = document.querySelector("#labComplete");
+const labAll = document.querySelector("#labAll");
 let taskList = []
 function inputTask(event) {
     event.preventDefault();
@@ -8,26 +11,54 @@ function inputTask(event) {
     if (info.length === 0) {
         return;
     } else {
-        taskList.push([info, false]);
-        RenderTask(taskList);
+        taskList.push({
+            id: Date.now(),
+            Name:info,
+            Status:false,
+        });
+        SaveTask();
+        RenderTaskCount();
+        Search();
     }
+    event.target.inpTask.value = "";
+}
+function ReadTask(){
+    let data = localStorage.getItem("taskList");
+    if(data){
+        taskList = JSON.parse(data);
+    }
+}
+function SaveTask(){
+    localStorage.setItem("taskList",JSON.stringify(taskList));
+}
+function RenderTaskCount() {
+    let active = taskList.filter(task => !task.Status).length;
+    let complete = taskList.filter(task=> task.Status).length;
+    let allTask = taskList.length;
+    labActive.textContent = active;
+    labComplete.textContent = complete;
+    labAll.textContent = allTask;
 }
 function Search() {
     let searchTarget = String(search.value).trim();
     if (searchTarget.length === 0) {
         RenderTask(taskList);
     } else {
-        taskFrame.replaceChildren();
         let temp = taskList.filter((task) => {
-            return String(task[0]).toLowerCase().includes(searchTarget.toLocaleLowerCase());
+            return String(task.Name).toLowerCase().includes(searchTarget.toLocaleLowerCase());
         });
-        console.log(temp);
         RenderTask(temp);
     }
 }
+function CheckboxEvent(event,task){
+    task.Status = event.target.checked;
+    SaveTask();
+    RenderTaskCount();
+    Search();
+}
 function RenderTask(Tasks) {
     taskFrame.replaceChildren();
-    Tasks.forEach((task, index) => {
+    Tasks.forEach((task) => {
         const taskCard = document.createElement("div");
         taskCard.classList.add("border", "rounded", "d-flex", "p-3", "justify-content-between", "mb-3");
 
@@ -35,26 +66,33 @@ function RenderTask(Tasks) {
         const Checkbox = document.createElement("input");
         Checkbox.type = "checkbox";
         Checkbox.classList.add("form-check-input");
-        Checkbox.addEventListener("change", (event) => { task[1] = event.target.checked; });
-        Checkbox.checked = task[1];
+        Checkbox.addEventListener("change", (event) =>{CheckboxEvent(event,task)});
+        Checkbox.checked = task.Status;
         const labTask = document.createElement("label");
-        labTask.textContent = task[0];
+        labTask.textContent = task.Name;
         labTask.classList.add("form-check-label", "ms-1");
         taskForm.append(Checkbox, labTask);
 
         const btnClose = document.createElement("button");
         btnClose.classList.add("btn", "btn-close");
-        btnClose.addEventListener("click", () => { RemoveTask(index) });
+        btnClose.addEventListener("click", () => { RemoveTask(task) });
         taskCard.append(taskForm, btnClose);
         taskFrame.append(taskCard);
     });
 }
-function RemoveTask(index) {
-    taskList.splice(index, 1);
-    RenderTask(taskList);
+function RemoveTask(target) {
+    taskList = taskList.filter((task)=>{
+        return task.id !== target.id;
+    });
+    SaveTask();
+    RenderTaskCount();
+    Search();
 }
 function main() {
     addForm.addEventListener("submit", inputTask);
     search.addEventListener("input", Search);
+    ReadTask();
+    Search();
+    RenderTaskCount();
 }
 main();
